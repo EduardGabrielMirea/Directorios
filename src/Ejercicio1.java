ba72a0f911cf96c0083af531af3fde328cd470a0
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-04-29 09:11:44 +0200

    update1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        File f = new File("Ejercicio1.txt");
+        System.out.println("Lista de ficheros");
+        System.out.println("------------------");
+
+    }
+}
