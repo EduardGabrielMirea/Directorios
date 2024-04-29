6b050e03db8dc4873289f83373b929806f5d92d9
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-04-29 10:24:01 +0200

    update3

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
         //Con File.listRoots()[0] nos devolvera la raiz porque la opcion 0 es el directorio padre.
-        File currentDirectory = File.listRoots()[0];
-        navigateDirectory(currentDirectory);
+        File Directory = File.listRoots()[0];
+        navigateDirectory(Directory);
     }
 
     public static void navigateDirectory(File directory) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            //Muestra con la ruta absouta es decir desde la raiz, e inserta los archivos en un array list.
             System.out.println("\nDirectorio actual: " + directory.getAbsolutePath());
             File[] files = directory.listFiles();
             int count = 1;
 
             System.out.println("0. Ir al directorio padre");
-
+            //Recorre el array list y lo imprime empezando desde "1 .".
             for (File file : files) {
+                //1 + .
                 System.out.print(count + ". ");
                 if (file.isDirectory()) {
                     System.out.println("<" + file.getName() + ">");
                 } else {
+                    //En caso de que sea un archivo, con file.length() podremos ver el tamaño del mismo.
                     System.out.println(file.getName() + " (" + file.length() + " bytes)");
                 }
                 count++;
             }
 
-            System.out.print("\nIngrese el número correspondiente: ");
+            System.out.print("\nIngrese el número correspondiente (-1 para salir): ");
             int choice = scanner.nextInt();
 
             if (choice == -1) {
@@ -43,6 +46,7 @@ public class Ejercicio1 {
                     System.out.println("Este directorio no tiene directorio padre.");
                 }
             } else if (choice > 0 && choice <= files.length) {
+                //Choice - 1 ya que empezamos en 0 pero nuesto menu empieza en 1, entonces hay que quitarle siempre 1 para que conicida con el menu.
                 File chosenFile = files[choice - 1];
                 if (chosenFile.isDirectory()) {
                     if (chosenFile.canRead()) {
--- src/Ejercicio1x.java
+++ src/Ejercicio1x.java
@@ -1,11 +1,47 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Ejercicio1x {
-    public static void main(String[] args) {
-        File directorioActual = File.listRoots()[0];
+    public static void main(String[] args){
+        //En sistemas Linux devolverá un único elemento, pero en sistemas Windows devolverá la raíz de cada unidad del sistema, por eso es un array. Esta es una manera de obtener un File que apunta a la raíz (y en el caso de Windows a la raíz de C :)
+        File f = File.listRoots()[0];
+        System.out.println("Lista de ficheros y directorios del directorio actual");
+        System.out.println("---------------------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        int contador;
+        int opcion;
+        do {
+            contador = 0;
+            opcion = 0;
+            for (File e : f.listFiles()) {
+                contador++;
+                if ( e.isFile()) {
+                    System.out.println(contador+". -"+e.getName() + " " + e.length());
+                }
+                if (e.isDirectory()){
+                    System.out.println(contador+". -"+e.getName()  + " <Directorio>");
+                }
+            }
+            try {
+                opcion = sc.nextInt();
+            }catch (InputMismatchException ime){
+                System.out.println("Introduzca sólo números");
+            }
+            sc.nextLine();
+            if (opcion == 0) {
+               if (f.getParentFile() != null) {
+                   f = f.getParentFile();
+               }
+            }else if (opcion !=  -1) {
+                if(opcion >= 0 && opcion <= f.listFiles().length){
+                    if (f.listFiles()[opcion - 1].isDirectory() && f.listFiles()[opcion - 1].canRead()){
+                        f = f.listFiles()[opcion - 1];
+                    }
+                }
+            }
+        }while (opcion != -1);
     }
 
-    public static void navegarDirectorios(){
-
-    }
 }
